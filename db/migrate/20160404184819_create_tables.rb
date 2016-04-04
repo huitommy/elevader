8e4e8e36f706aa4e31c01c9169d5f1654e2da1d1
Filipe <filipe.m.mir@gmail.com>
2016-04-04 15:53:10 -0400

    Addressed whitespace issue in create_tables.rb

--- db/migrate/20160404184819_create_tables.rb
+++ db/migrate/20160404184819_create_tables.rb
@@ -2,7 +2,7 @@ class CreateTables < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.string :email, null: false 
+      t.string :email, null: false
     end
 
     add_index :users, :email, unique: true
