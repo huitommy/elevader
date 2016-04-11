1a0653752c77e55d9397f86156a27b8b82da3ebb
Filipe <filipe.m.mir@gmail.com>
2016-04-11 10:09:47 -0400

    Added basic structure of tests

---
+++ spec/features/user_votes_for_reviews_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature 'Voting buttons for reviews: ' do
+
+  scenario 'Admins can see voting buttons and vote total' do
+    
+  end
+
+  scenario 'Admins cannot upvote or downvote' do
+  end
+
+  scenario 'Logged-out user can see voting buttons and vote total' do
+  end
+
+  scenario 'Logged-out user is prompted to sign-in if she attempts to vote' do
+  end
+
+  scenario 'Logged-in user can see voting buttons and vote total' do    
+  end
+
+  scenario 'Logged-in user can vote up once' do
+  end
+
+  scenario 'Logged-in user can vote down once' do
+    
+  end
+
+  scenario 'Clicking the same vote button twice erases the vote' do
+    
+  end
+
+  scenario 'Clicking the opposite vote button changes the direction of the vote' do
+    
+  end
+
+  scenario 'The vote totals number displays the sum of all votes across users' do
+    
+  end
+  
+end
