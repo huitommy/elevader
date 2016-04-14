9de0174f37b1eb0f2d4af7ed677fadf18e2bd5fa
Tommy Hui <hui.1tommy@gmail.com>
2016-04-14 11:42:14 -0400

    fixed hound problems

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -7,6 +7,6 @@ CarrierWave.configure do |config|
       aws_access_key_id: ENV['S3_KEY'],
       aws_secret_access_key: ENV['S3_SECRET']
   }
-  config.fog_directory  = ENV['S3_BUCKET']
+  config.fog_directory = ENV['S3_BUCKET']
   config.fog_public = false
 end
