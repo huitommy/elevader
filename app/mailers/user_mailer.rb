eb74657e2ac1b5eff4f901bbf1c1f1c1b9aed9e3
Will <will@resilientcoders.org>
2016-04-14 14:36:16 -0400

    fixed hound issues

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   def added_review(user, elevator)
     @user = user
     @elevator = elevator
-    mail(to: user.name_to_s,
+    mail(to: "#{@user.email}",
          subject: 'Your elevator has a new Review!')
   end
 end
